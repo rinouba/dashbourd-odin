cbacf16d938843cc5f6ec8c86fb6ee5a7a854e88
Oussama Rinobua <oussamarinouba@gmail.com>
2023-09-29 23:14:55 +0200

    add a button icon for a responsive

---
+++ index.js
@@ -0,0 +1,11 @@
+//  GET /trips
+// GET /stations
+//  and a third one of your chouce
+function myFunction() {
+  var x = document.querySelector(".sidebar");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
